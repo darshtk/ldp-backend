206e5073bcb16d135355b121b8724779f4c8821a
Darshana <43498943+darshtk@users.noreply.github.com>
2026-03-22 11:47:08 +0530

    Create Spring Boot main application class

---
+++ src/main/java/com/ldp/LdpBackendApplication.java
@@ -0,0 +1,12 @@
+package com.ldp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdpBackendApplication.class, args);
+    }
+}
